79b80a8aa266d53c2622615c960e9a214462a528
Jagjeet Saini <jagjeetsaini@Jagjeets-MacBook-Air.local>
2023-05-02 16:03:33 +0530

    first commit

---
+++ second-highest.js
@@ -0,0 +1,11 @@
+// To find the second largest number in an array.
+/*
+Method 1:
+step1 : loop over array and remove the largest element.
+step2: loop over remaining array and then find the largest.
+
+Method 2:
+
+
+
+*/
